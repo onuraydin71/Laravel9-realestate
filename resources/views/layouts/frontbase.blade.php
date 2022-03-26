5cefa9119c11ea1e9177f86d280b68e4d92b39cb
onuraydin71 <onura1903@gmail.com>
2022-03-26 14:02:10 +0300

    Page Changed

---
+++ resources/views/layouts/frontbase.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <title>Real Estate</title>
+    
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
+    <!-- Font awesome -->
+    <link href="{{asset('assets')}}/css/font-awesome.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="{{asset('assets')}}/css/bootstrap.css" rel="stylesheet">   
+    <!-- slick slider -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/slick.css">
+    <!-- price picker slider -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/nouislider.css">
+    <!-- Fancybox slider -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/jquery.fancybox.css" type="text/css" media="screen" /> 
+    <!-- Theme color -->
+    <link id="switcher" href="{{asset('assets')}}/css/theme-color/default-theme.css" rel="stylesheet">     
+
+    <!-- Main style sheet -->
+    <link href="{{asset('assets')}}/css/style.css" rel="stylesheet">    
+
+   
+    <!-- Google Font -->
+    <link href='{{asset('assets')}}/https://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>    
+    <link href='{{asset('assets')}}/https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+        @yield("head")
+  </head>
+  <body class="aa-price-range"> 
+
+  @include("home.header")
+
+        @section('sidebar')
+        @include("home.sidebar")
+        
+        @show
+ 
+        <div class="container">
+            @yield('content')
+        </div>
+
+        @include("home.footer")
+        @yield('foot')
+    </body>
+</html>
